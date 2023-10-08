e5b5ba6d34b7e2c07133ecb076a79807f28e93e2
Kevin Sinchi <kevinjss97@gmail.com>
2023-10-08 19:22:15 +0200

    :tada: Init backend Dockefile
    Dockerfile and get for test

--- backend-Spring/src/main/java/backend/api/ApiApplication.java
+++ backend-Spring/src/main/java/backend/api/ApiApplication.java
@@ -2,12 +2,26 @@ package backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+
+	@GetMapping("/test")
+	public String test(@RequestParam(value = "name", defaultValue="Wordl") String name) {
+		return String.format("test %s!", name);
+	}
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+    }
 
 }
